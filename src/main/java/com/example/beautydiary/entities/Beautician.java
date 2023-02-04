48c72880aebfaaaed071cbcca146170ad973bfef
YOUR NAMEAlena <alena.yefymenko@gmail.com>
2023-02-04 21:00:50 +0200

    Merge remote-tracking branch 'origin/main'

--- src/main/java/com/example/beautydiary/entities/Beautician.java
+++ src/main/java/com/example/beautydiary/entities/Beautician.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 @NoArgsConstructor
@@ -22,10 +23,14 @@ public class Beautician {
     @Column(name = "phone_number")
     private String phoneNumber;
     private String location;
-    private String category;
+    @ManyToOne
+    private Category category;
     private String password;
     @Column(name = "profile_picture_URL")
     private String profilePictureURL;
+    private String aboutMe;
+
+    @CreationTimestamp
     private java.sql.Timestamp createdAt;
     @UpdateTimestamp
     private java.sql.Timestamp updatedAt;
