f6f4e0b4d6abde770c9999de3878a52422f4c1cd
YOUR NAMEAlena <alena.yefymenko@gmail.com>
2023-02-20 21:09:05 +0200

    handle error print

--- src/main/java/com/example/beautydiary/controllers/UserController.java
+++ src/main/java/com/example/beautydiary/controllers/UserController.java
@@ -5,9 +5,11 @@ import com.example.beautydiary.entities.User;
 import com.example.beautydiary.services.UserService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,21 +24,24 @@ public class UserController {
         this.userService = userService;
     }
     @GetMapping("/register")
-    public String showRegistrationPage(){
+    public String showRegistrationPage(Model model){
+        model.addAttribute("user", new User());
         return "register";
     }
 
     @PostMapping("/register")
-    public String handleUserRegistration(User user, Model model){
-        try {
-           userService.createUser(user);
-        } catch (Exception e) {
-            model.addAttribute("message","signup_failed");
-            model.addAttribute("error",e.getMessage());
-            model.addAttribute("user", user);
+    public String handleUserRegistration(@Valid User user, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
             return "register";
         }
-
+        try {
+            userService.createUser(user);
+        } catch (Exception e) {
+            model.addAttribute("message", "signup_failed");
+            model.addAttribute("error", e.getMessage());
+            model.addAttribute("user", new User());
+            return "register";
+        }
         return "redirect:login?message=signup_success";
     }
 
@@ -47,24 +52,30 @@ public class UserController {
         model.addAttribute("message", message);
         return "login";
     }
-
     @PostMapping("/login")
     public String handleUserLogin(User user, HttpServletResponse response, Model model){
         try {
             User loggedInUser = userService.verifyUser(user);
-            Cookie cookie= new Cookie("userId",loggedInUser.getId().toString());
-            response.addCookie(cookie);
-            response.addCookie(new Cookie("userIsLoggedIn","true"));
+            Cookie userIdCookie= new Cookie("userId",loggedInUser.getId().toString());
+            userIdCookie.setPath("/");
+            userIdCookie.setMaxAge(3600); // 1 hour
+
+            Cookie userIsLoggedInCookie= new Cookie("userIsLoggedIn","true");
+            userIsLoggedInCookie.setPath("/");
+            userIsLoggedInCookie.setMaxAge(3600); // 1 hour
+
+            response.addCookie(userIdCookie);
+            response.addCookie(userIsLoggedInCookie);
+
             model.addAttribute("user", loggedInUser);
-            if(Objects.equals(loggedInUser.getUserType(), "beautician")) {
-                return "redirect:/master-account/"+loggedInUser.getId();
-            }else {
-                return "/home";
+            if (Objects.equals(loggedInUser.getUserType(), "beautician")) {
+                return "redirect:/master-account/" + loggedInUser.getId();
+            } else {
+                return "redirect:/home";
             }
         }catch (Exception e){
             return "redirect:login?message=login_failed&error="+ e.getMessage();
         }
-
     }
 
 }
--- src/main/java/com/example/beautydiary/entities/User.java
+++ src/main/java/com/example/beautydiary/entities/User.java
@@ -1,6 +1,10 @@
 package com.example.beautydiary.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,13 +22,32 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", updatable = false, nullable = false)
     private Long id;
+
+
     @Column(name = "full_name")
+    @Size(min = 10, max = 50, message = "Full name must be between {min} and {max} characters")
+    @Pattern(regexp = "^[a-zA-Z ]+$", message = "Full name must contain only letters")
     private String fullName;
+
+
+    @NotBlank(message = "Email cannot be empty")
+    @Email(message = "Invalid email address")
+    @Pattern(regexp = "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}", message = "Invalid email format")
     @Column(unique = true)
     private String email;
+
+    @NotBlank
+    @Size(min = 8, max = 8)
+    @Pattern(regexp = "\\d{8}", message = "Phone number must contain only digits")
     @Column(name = "phone_number")
     private String phoneNumber;
+
+    @Pattern(regexp = "^[a-zA-Z]{8,20}$", message = "Password should contain only latin letters and be between 8 and 20 characters long")
+    @Size(min = 8, max = 20, message = "Password should be between 8 and 20 characters long")
+    @NotBlank(message = "Password is required")
     private String password;
+
+
     @CreationTimestamp
     private Timestamp createdAt;
     @UpdateTimestamp
