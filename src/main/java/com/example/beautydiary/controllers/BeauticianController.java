b8bcd6aadd492195a157d8d2527a2b2cf0af7fbf
YOUR NAMEAlena <alena.yefymenko@gmail.com>
2023-02-21 12:17:41 +0200

    added common master profile page

--- src/main/java/com/example/beautydiary/controllers/BeauticianController.java
+++ src/main/java/com/example/beautydiary/controllers/BeauticianController.java
@@ -1,13 +1,14 @@
 package com.example.beautydiary.controllers;
 
 import com.example.beautydiary.entities.Beautician;
+import com.example.beautydiary.entities.PriceListItem;
 import com.example.beautydiary.entities.Reservation;
 import com.example.beautydiary.services.BeauticianService;
 
+import com.example.beautydiary.services.MasterAccountService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 
@@ -16,10 +17,12 @@ import java.util.List;
 @Controller
 public class BeauticianController {
     private BeauticianService beauticianService;
+    private MasterAccountService mas;
 
 
-    public BeauticianController(BeauticianService beauticianService) {
+    public BeauticianController(BeauticianService beauticianService, MasterAccountService mas) {
         this.beauticianService = beauticianService;
+        this.mas = mas;
     }
 
     @GetMapping("/categories/{categoryId}/beauticians")
@@ -35,6 +38,11 @@ public class BeauticianController {
         model.addAttribute("beautician", beautician);
         var reservation = new Reservation();
         reservation.setBeautician(beautician);
+        List<PriceListItem> itemList = mas.getAllByBeauticianId(id);
+        PriceListItem item = new PriceListItem();
+        item.setBeautician(beautician);
+        model.addAttribute("itemList", itemList);
+        model.addAttribute("item", item);
         model.addAttribute("reservation", reservation);
         return "beautician";
     }
